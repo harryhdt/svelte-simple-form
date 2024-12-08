ed4ca6d6cff9896c84d3e169eb4429248333432f
Harry H <harryhdtofficial@gmail.com>
2024-12-08 11:39:20 +0800

    example: add nested data

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -13,7 +13,15 @@
 					label: z.string()
 				})
 			)
-			.default([])
+			.default([]),
+		nested: z.object({
+			a: z.string().min(1),
+			b: z.string(),
+			c: z.object({
+				c1: z.string(),
+				c2: z.string().min(1, 'c2 is required, this is custom message')
+			})
+		})
 	});
 
 	type DataType = z.infer<typeof schema>;
@@ -23,7 +31,15 @@
 			name: 'Harry',
 			age: 18,
 			hobbies: [],
-			tags: []
+			tags: [],
+			nested: {
+				a: '',
+				b: '',
+				c: {
+					c1: '',
+					c2: ''
+				}
+			}
 		},
 		schema,
 		onSubmit: async (data) => {
@@ -37,23 +53,25 @@
 	});
 </script>
 
-<div>
+<div style="font-size: 15px;font-family: Verdana, Geneva, Tahoma, sans-serif;margin: 20px 10px;">
 	<form use:enhance>
-		Form: <pre>{JSON.stringify(form, null, 2)}</pre>
+		svelte-simple-form <pre style="font-size: 10px;">{JSON.stringify(form, null, 2)}</pre>
 		<br /><br />
-		Error:
-		<pre>{JSON.stringify(form.errors, null, 2)}</pre>
+		<hr />
 		<br /><br />
+		Form
+		<br />
+		<br />
 		<div>
 			<input type="text" bind:value={form.data.name} />
 			{#if form.errors?.name}
-				<p>{form.errors?.name?.join(', ')}</p>
+				<p style="font-size: 12px;color: red">{form.errors?.name?.join(', ')}</p>
 			{/if}
 		</div>
 		<div>
 			<input type="number" bind:value={form.data.age} />
 			{#if form.errors?.age}
-				<p>{form.errors?.age?.join(', ')}</p>
+				<p style="font-size: 12px;color: red">{form.errors?.age?.join(', ')}</p>
 			{/if}
 		</div>
 		<span>Hobbies</span>
@@ -135,6 +153,14 @@
 			/>
 			<span>Man</span>
 		</label>
+		<br />
+		<br />
+		<div>
+			<input type="text" bind:value={form.data.nested.c.c2} placeholder="c2" />
+			{#if form.errors?.nested.c.c2}
+				<p style="font-size: 12px;color: red">{form.errors?.nested.c.c2?.join(', ')}</p>
+			{/if}
+		</div>
 		<br />
 		<br />
 		<button type="button" onclick={() => form.reset()}>Reset</button>
