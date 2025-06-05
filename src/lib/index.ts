96382ec3180ab787634e512b9285d7a1c95f1314
devgauravjatt <devgauravjatt@gmail.com>
2025-06-05 20:19:41 +0530

    ✨ make better and v0.2.5
    🚀feat: add method  setError method
    
    Allows manual error setting for fields.
    
    Provides more control over error messages.
    
    📝 docs: update docs for setError
    
    website on live demo and code example add
    
    Added documentation and example for setError.
    
    ℹ️ fix: fix dev Primitive eslint config error
    
    🎉 release: v0.2.5

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,1 @@
-export { default as useForm } from './form.svelte.js';
+export { default as useForm } from './form.svelte';
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import '../app.css';
+
+	let { children } = $props();
+</script>
+
+<main class="mt-12">
+	{@render children()}
+</main>
